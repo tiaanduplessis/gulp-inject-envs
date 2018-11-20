f7f948cec67140c148dbc08cc61b902caba46cc8
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-11-20 10:05:26 +0200

    hotfix/add-delimiters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const replaceTask = require('gulp-replace-task')
 
 module.exports = function injectEnvs (envVars = {}, { prefix = 'ENV::' } = {}) {
-  const pattern = `${prefix}(.*?)`
+  const pattern = `<${prefix}(.*?)>`
   const regex = new RegExp(pattern, 'g')
 
   return replaceTask({
